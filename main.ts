b5480c524468bc8a71fc06ece06e9023e274357f
Yoshio HANAWA <y@hnw.jp>
2024-02-13 14:20:41 +0900

    Apply @hono/oidc-auth

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 import { Hono } from 'npm:hono'
 import { serveStatic } from 'npm:hono/deno'
+import { oidcAuthMiddleware, getAuth, revokeSession } from 'npm:@hono/oidc-auth';
 
 const app = new Hono()
 
-app.use('/*', serveStatic({ root: 'public/' }))
-app.get('/hello', (c) => {
-  return c.text('Hello Hono!')
+app.get('/logout', async (c) => {
+  await revokeSession(c)
+  return c.text('You have been successfully logged out!')
 })
+app.use('/*', oidcAuthMiddleware())
+app.use('/*', async (c, next) => {
+  const auth = await getAuth(c)
+  if (!auth?.email.endsWith('@hnw.jp')) {
+    return c.text('Unauthorized', 401)
+  }
+  await next()
+})
+app.use('/*', serveStatic({ root: 'public/' }))
 
 Deno.serve(app.fetch)
